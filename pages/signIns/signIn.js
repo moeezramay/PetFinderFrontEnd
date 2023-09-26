ab8a3a9612708d140712e09aafe34ee5230db838
Asher Hayat <asherhayat85@gmail.com>
2023-09-26 17:06:50 +0500

    V

--- pages/signIns/signIn.js
+++ pages/signIns/signIn.js
@@ -1,18 +1,86 @@
-import React from "react";
+import React from 'react';
 import "./sign.css";
-import Waves from "../../public/waves";
-import Logo from "../../public/signInlogo";
-import TouchHand from "../../public/handTouchsvg";
-import ShareIcon from "../../public/shareIconsvg";
+import Waves from "./../../public/waves"; // Adjusted import
+import Logo from "./../../public/signInlogo"; // Adjusted import
+import TouchHand from "./../../public/handTouchsvg"; // Adjusted import
+import ShareIcon from "./../../public/shareIconsvg"; // Adjusted import
 import { useRouter } from "next/navigation";
 
-function SignIn() {
-    var router = useRouter();
+// Rest of your code...
 
-    var shiftPage = (link) => {
-        router.push(link);
-    };
-    return <div className="parent">Hello js</div>;
+function signIn() {
+  var router = useRouter();
+
+  var shiftPage = (link) => {
+    router.push(link);
+  }
+  //shiftPage("/signup");
+  return (
+    <form>
+    <div className="parent">                                                          {/*Parent Element that holds the entire page */}
+      <div className='centerIt'>                                                      {/* For centering the box*/}
+        <div className='aboutSectionHeading' maxWidth="lg" minWidth="sm">       {/* div for the entire box of signup and about section*/}
+
+          <div className='aboutSectiondiv'  data-testid='aboutSectiondiv'>   {/* About section*/}       
+            <div variant='h4' className='aboutHeading'>One Platform!</div>
+            <div variant='h6' className='aboutDescription'>For Finding Lost Pets!</div>
+            <div className='OwnersSection'>     {/*Owner Section Start */}
+              <div className='ParentOwner'>   
+                <div variant='h5' color='#f6fdff' fontWeight='bold'>Owners!</div>
+                <div className='OwnerDescription'>
+                  <div variant='h7' color='#f6fdff'>Find your lost pet</div>
+                  <div variant='h7' color='#f6fdff'>within a few clicks!</div>
+                </div>
+              </div>
+              <div className='HandLogo'>
+                <TouchHand />
+              </div>
+            </div>                            {/*Owner Section End */}
+
+            <div className='LostPetsSection'>     {/*Lost Pets Section Start */}
+              <div className='ParentOwner'>   
+                <div variant='h5' color='#f6fdff' fontWeight='bold'>Lost Pets!</div>
+                <div className='OwnerDescription'>
+                  <div variant='h7' color='#f6fdff'>Register any lost</div>
+                  <div variant='h7' color='#f6fdff'>pets to find its owner</div>
+                </div>
+              </div>
+              <div className='ShareLogo'>
+                <ShareIcon />
+              </div>
+            </div>                        {/*Lost Pets Section End */}
+          </div>
+
+          <div className='signUpSectiondiv'>                                    {/* Signup section*/}
+            <div className='logoAndSignUp'>
+              <Logo  className='signUpLogo'/>
+              <div variant='h5' className='signUpHeading'>SignIn</div>
+            </div>
+            <div className='EmailAndPasswordAndForgotPass'>                          {/* Email, Password, ForgotPassword, Button, Dont have an account section*/}
+              <div variant='subtitle2' className='EmailHeading'>Email</div>
+              <input label="Enter your email" type='email' variant='outlined' size='small' className='Emailinput' />
+              <div variant='subtitle2' className='PassHeading'>Password</div>
+              <input label="Enter your Password" type='password' variant='outlined' size='small' className='Passinput' />
+              <link href="https://www.example.com" target="_blank" rel="noopener noreferrer">  {/* Add link To Forgot Password Page */}
+                <div variant='subtitle2' className='ForgotPassHeading'>Forgot Password?</div> 
+              </link>
+              <div className='SeperatingLine'></div>
+              <button variant="contained" className="signInButton">SignIn</button>
+              <div className='DontHaveAnAccountdiv'>
+                <div variant='subtitle2' className='DontHaveAnAccount'>Dont have an account?</div>
+                <link href="/signup" rel="noopener noreferrer">   {/* Add link To signIn Page */}
+                  <div variant='subtitle2' className='SignUp'>SignUp</div>
+                </link>
+              </div>            
+            </div>
+          </div>
+
+        </div>
+        <Waves className="waves" />
+      </div>
+    </div>
+    </form>
+  );
 }
 
-export default SignIn;
+export default signIn;
--- pages/signUp/signup.js
+++ pages/signUp/signup.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import "./signup.css";
 import Waves from "../../public/waves";
 import Logo from "../../public/signInlogo";
@@ -6,18 +6,69 @@ import TouchHand from "../../public/handTouchsvg";
 import ShareIcon from "../../public/shareIconsvg";
 import { useRouter } from "next/navigation";
 
-function SignIn() {
-    var router = useRouter();
 
-    var shiftPage = (link) => {
-        router.push(link);
-    };
-    //shiftPage("/signup");
-    return (
-        <div className="parent">
-            <div>Bruhh</div>
+function SignUp() {
+  var router = useRouter();
+
+  var shiftPage = (link) => {
+    router.push(link);
+  }
+  //shiftPage("/signup");
+  return (
+    <form>
+    <div className="parent">                                                          {/*Parent Element that holds the entire page */}
+      <div className='centerIt'>                                                      {/* For centering the box*/}
+        <div className='aboutSectionHeading' maxWidth="lg" minWidth="sm">  
+               
+       {/* div for the entire box of signup and about section*/}
+        <div className='aboutSectiondiv'  data-testid='aboutSectiondiv'>   {/* About section*/}  
+        <div   className='primarySignUpLogo'> <Logo /></div>
+        <div className='name'>Name</div>
+        <input placeholder="Enter your Name" type='phoneNo' variant='outlined' size='small' className='EmailTextField'/>
+        <div className='password'>Password</div>
+        <input placeholder="Enter your Password" type='phoneNo' variant='outlined' size='small' className='EmailTextField' />
+        <div className='confirmPassword'>Confirm Password</div>
+        <input placeholder="Confirm your password" type='phoneNo' variant='outlined' size='small' className='EmailTextField' />
+        <div className='tradeMark'> ©MatDevs - 2023 - All Rights Reserved</div>
+                                {/*Lost Pets Section End */}
+          </div>
+          <div className='signUpSectiondiv'>                                    {/* Signup section*/}
+            <div className='logoAndSignUp'>   
+              <div variant='h5' className='signUpHeading'>SignUp</div>
+            </div>
+            <div className='EmailAndPasswordAndForgotPass'>                          {/* Email, Password, ForgotPassword, Button, Dont have an account section*/}
+              <div variant='subtitle2' className='EmailHeading'>Email</div>
+              <input placeholder="Enter your Email" type='phoneNo' variant='outlined' size='small' className='EmailTextField'/>
+              <div variant='subtitle2' className='PassHeading'>Phone</div>
+              <input placeholder="Enter your Phone No." type='phoneNo' variant='outlined' size='small' className='PassTextField' />
+              <div className='Gender'>Gender</div>
+              <div class="checkbox-div">
+              <label>
+              <input type="checkbox"/>Male     
+              </label>
+              <label>
+              <input type="checkbox"/> Female
+              </label>
+              <label>
+              <input type="checkbox"/> Other
+              </label>
+              </div>
+              <button  className="signUpButton"  >SignUp</button>
+              <div className='DontHaveAnAccountdiv'>
+                <div variant='subtitle2' className='DontHaveAnAccount'>Already have an account?</div>
+                <link href="/signIn" rel="noopener noreferrer">   {/* Add Link To signIn Page */}
+                  <div variant='subtitle2' className='SignUp' href=''>SignIn</div>
+                </link>
+              </div>            
+            </div>
+          </div>
+
         </div>
-    );
+        <Waves className="waves" />
+      </div>
+    </div>
+    </form>
+  );
 }
 
-export default SignIn;
+export default SignUp;
