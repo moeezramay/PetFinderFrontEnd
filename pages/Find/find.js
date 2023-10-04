7f8d1432b4a8a1ac47426e2b25d03090e160c80d
Moeez Ramay <moeezramay@192.168.0.140>
2023-10-04 17:00:23 +0500

    Find Page design completed but not responsive

--- pages/Find/find.js
+++ pages/Find/find.js
@@ -13,13 +13,42 @@ export default function Find() {
         </div>
         <div className="SepratingLine-find"></div>
         <div className="filter-button-container-find">
-        <button className="filter-button-find" type="submit">
-          Filter
-        </button>
+          <button className="filter-button-find" type="submit">
+            Add Filter's
+          </button>
         </div>
         <div className="SepratingLine-find"></div>
       </div>
       <div className="pet-results-find">Showing Latest Results For Reported Pets:</div>
+      <div className="results-parent-find">
+        <div className="card-results-find">
+          <div className="img-card-find"> Image </div>
+          <div className="line-box-find"></div>
+          <div style={{ marginTop:'6%',color:'white',marginLeft:'8%'}}>
+            <div style={{display:'flex'}}>
+              <div className="pet-info-find">Pet Name: </div>
+              <div className="pet-answer-find">Smokey</div>
+            </div>
+            <div style={{display:'flex',marginTop:"5%"}}>
+              <div className="pet-info-find">Fur Color:</div>
+              <div className="pet-answer-find">Grey</div>
+            </div>
+            <div style={{display:'flex',marginTop:"5%"}}>
+              <div className="pet-info-find">Eye Color:</div>
+              <div className="pet-answer-find">Blue</div>
+            </div>
+            <div style={{display:'flex',marginTop:"5%"}}>
+              <div className="pet-info-find">City:</div>
+              <div className="pet-answer-find">Lahore</div>
+            </div>
+          </div>
+
+          <div className="view-button-container-find">
+            <button className="view-button-find">See Details</button>
+          </div>
+        </div>
+        
+      </div>
     </form>
   );
 }
